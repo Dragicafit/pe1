07011d7714bee4a56480f1a324bcca11208bd43b
antoine berthel <antoine.berthel@hotmail.com>
2020-02-12 09:59:28 +0100

    Z

--- TP4/src/Somme.java
+++ TP4/src/Somme.java
@@ -1,15 +1,25 @@
 
 public class Somme {
-	
-	int s;	// Somme à atteindre
-	int k;	// Pièces utilisés
-	int[] pieces;	// Pièces
-	int[][] z;
-	
+
+	int s; // Somme à atteindre
+	int k; // Pièces utilisés
+	int[] pieces; // Pièces
+	long[][] z;
+
 	public Somme(int s, int k) {
 		this.s = s;
 		this.k = k;
 		this.pieces = new int[k];
-		this.z = new int[s+1][k+1];
+		this.z = new long[s + 1][k + 1];
+		initZ();
+	}
+
+	public void initZ() {
+		for (int i = 0; i <= s; i++) {
+			this.z[i][0] = -1;
+		}
+		for (int i = 0; i <= k; i++) {
+			this.z[0][i] = 0;
+		}
 	}
 }
