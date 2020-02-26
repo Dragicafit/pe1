27e99e71b34830872c23eb820fe0fe627cd525bd
Ake <cedric_ji@yahoo.fr>
2020-02-26 10:23:28 +0100

    Parser ended

--- TP5/src/Parser.java
+++ TP5/src/Parser.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Parser {
@@ -11,7 +12,50 @@ public class Parser {
 	}
 	
 	public void parse() {
-		Scanner sc = new Scanner(new File(filepath));
-		int[] size = new int[5];
+		Scanner sc = null;
+		try {
+			sc = new Scanner(new File(filepath));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		int nbV = sc.nextInt();
+		int nbEp = sc.nextInt();
+		int nbR = sc.nextInt();
+		int nbC = sc.nextInt();
+		int cacheSize = sc.nextInt();
+		
+		Network network = new Network();
+		network.center = new DataCenter();
+		
+		// Videos
+		
+		for(int i = 0; i < nbC; i++) {
+			network.caches.add(new Cache());
+			network.caches.get(i).max_size = cacheSize;
+		}
+		
+		for(int i = 0; i < nbV; i++) {
+			network.center.videos.put(i, sc.nextInt());
+		}
+		
+		for(int i = 0 ; i < nbEp; i++) {
+			int latency = sc.nextInt();
+			int epCache = sc.nextInt();
+			
+			network.endPoints.add(new EndPoint());
+			network.center.endpoints.put(network.endPoints.get(i), latency);
+			
+			for(int j = 0; j < epCache; j++) {
+				Cache c = network.caches.get(sc.nextInt());
+				c.endpoint.put(network.endPoints.get(i), sc.nextInt());
+			}
+		}
+		
+		for(int i = 0; i < nbR; i++) {
+			int video = sc.nextInt();
+			EndPoint ep = network.endPoints.get(sc.nextInt());
+			ep.request.put(video, sc.nextInt());
+		}
 	}
 }
