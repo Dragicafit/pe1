212ca78ed4dcb1fc5e5b2b190a3e89b1da82ec59
Ake <cedric_ji@yahoo.fr>
2020-02-26 10:42:45 +0100

    First try

--- TP5/src/Main.java
+++ TP5/src/Main.java
@@ -1,7 +1,8 @@
+import java.io.IOException;
 
 public class Main {
-	public static void main(String[] args) {
-		Parser p = new Parser("input.in");
-		p.parse();
+	public static void main(String[] args) throws IOException {
+		Network n = Parser.parse("input.in");
+		Output.output(n.caches, "test.out");
 	}
 }
