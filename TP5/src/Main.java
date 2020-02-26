2dee81a6ff9250fc38cd873520a9c7e474b00cac
antoine berthel <antoine.berthel@hotmail.com>
2020-02-26 10:27:34 +0100

    test remplissage main

--- TP5/src/Main.java
+++ TP5/src/Main.java
@@ -1,6 +1,23 @@
+import java.util.ArrayList;
+import java.util.Map;
 
 public class Main {
 	public static void main(String[] args) {
-		
+		NetWork n = Parser.parser();
+	}
+	
+	public static void remplissage(Network n) {
+		for (EndPoint e : n.endPoints) {
+			for (Map.Entry<Integer, Integer> entry : e.request.entrySet()) {
+				for (Cache c : n.caches) {
+					Integer poids = n.center.videos.get(entry.getValue());
+					if (poids < c.max_size - c.size) {
+						c.videos.put(entry.getKey(), poids);
+						c.size += poids;
+						break;
+					}
+				}
+			}
+		}
 	}
 }
