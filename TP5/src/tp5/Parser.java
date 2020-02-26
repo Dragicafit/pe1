6b79a13daff27ef04057a5a4ec7704a7e7c1c0be
Ake <cedric_ji@yahoo.fr>
2020-02-26 09:20:47 +0100

    v0

---
+++ TP5/src/tp5/Parser.java
@@ -0,0 +1,19 @@
+package tp5;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Parser {
+	DataCenter data;
+	String filepath;
+	
+	public Parser(String filepath) {
+		data = new DataCenter();
+		this.filepath = filepath;
+	}
+	
+	public void parse() {
+		Scanner sc = new Scanner(new File(filepath));
+		int[] size = new int[5];
+	}
+}
