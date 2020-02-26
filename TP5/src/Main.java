7d4aa0b7bec1b16565a08c57b8b016f4aee5c942
antoine berthel <antoine.berthel@hotmail.com>
2020-02-26 10:36:09 +0100

    main throw

--- TP5/src/Main.java
+++ TP5/src/Main.java
@@ -1,11 +1,11 @@
+import java.io.IOException;
 import java.util.Map;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Network n = Parser.parse(args[0]);
 		remplissage(n);
-		Output.output(n.caches,"test.txt");
-		
+		Output.output(n.caches, "test.txt");
 	}
 	
 	public static void remplissage(Network n) {
