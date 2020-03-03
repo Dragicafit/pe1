e9e30ff38b4b22c797ce4eb971dae14bbff9e31b
tompayet <tompayet@mbp-de-tom>
2020-03-03 19:49:04 +0100

    Liste

--- TP5/src/Main.java
+++ TP5/src/Main.java
@@ -1,15 +1,18 @@
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		Network n = Parser.parse("input.in");
-		Output.output(n.caches, "test.out");
+		Network n = Parser.parse("kittens.in");
+		Commun(n);
+		Output.output(n.caches, "kittens.out");
 	}
 	public static void Commun(Network n) {
 		for(Cache c : n.caches) {
 			HashMap<Video, Integer> liste= new HashMap<>();
+			ArrayList<Request> l = new ArrayList<Request>();
 			for(Liaison li: c.liaisons) {
 				for(Request r : li.endpoint.requests) {
 					if(liste.containsKey(r.video)) {
@@ -20,10 +23,12 @@ public class Main {
 				}
 			}
 			for(Map.Entry<Video, Integer> elem : liste.entrySet()) {
-				if(elem.getKey().poids + c.size < c.max_size) {
+				if(elem.getKey().poids + c.size <= c.max_size) {
 					c.videos.add(elem.getKey());
+					c.size += elem.getKey().poids;
 				}
 			}
 		}
 	}
+	
 }
