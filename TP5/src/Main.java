0578943dd461834c563a06458309375e701734c2
Antoine <antoine.berthel@hotmail.com>
2020-03-03 21:24:49 +0100

    modifs

--- TP5/src/Main.java
+++ TP5/src/Main.java
@@ -1,34 +1,44 @@
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		Network n = Parser.parse("kittens.in");
+		Network n = Parser.parse("videos_worth_spreading.in");
 		Commun(n);
-		Output.output(n.caches, "kittens.out");
+		Output.output(n.caches, "videos_worth_spreading.out");
+		System.out.println("fini");
 	}
+
 	public static void Commun(Network n) {
-		for(Cache c : n.caches) {
-			HashMap<Video, Integer> liste= new HashMap<>();
+		for (Cache c : n.caches) {
 			ArrayList<Request> l = new ArrayList<Request>();
-			for(Liaison li: c.liaisons) {
-				for(Request r : li.endpoint.requests) {
-					if(liste.containsKey(r.video)) {
-						liste.put(r.video, r.nbDemandes+(liste.get(r.video)));
-					}else {
-						liste.put(r.video, r.nbDemandes);
+			for (Liaison li: c.liaisons) {
+				for (Request r : li.endpoint.requests) {
+					if (l.isEmpty()) {
+						l.add(r);
+					} else if (l.contains(r)) {
+						l.get(l.indexOf(r)).nbDemandes += r.nbDemandes;
+					} else {
+						boolean b = false;
+						for (Request rq : l) {
+							if (rq.nbDemandes > r.nbDemandes) {
+								l.add(l.indexOf(rq), r);
+								b = true;
+								break;
+							}
+						}
+						if (!b)
+							l.add(r);
 					}
 				}
 			}
-			for(Map.Entry<Video, Integer> elem : liste.entrySet()) {
-				if(elem.getKey().poids + c.size <= c.max_size) {
-					c.videos.add(elem.getKey());
-					c.size += elem.getKey().poids;
+			for (Request rq : l) {
+				if (!c.videos.contains(rq.video) && rq.video.poids + c.size <= c.max_size) {
+					c.videos.add(rq.video);
+					c.size += rq.video.poids;
 				}
 			}
 		}
 	}
-	
+
 }
